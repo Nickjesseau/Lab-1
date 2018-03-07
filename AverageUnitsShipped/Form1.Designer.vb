845f852f49419b66ad656abbc8954e7fe55d98e1
Nick Jesseau <Nick Jesseau@DESKTOP-896D841>
2018-03-07 01:09:34 -0500

    Add project files.

---
+++ AverageUnitsShipped/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ftmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.tbUnits = New System.Windows.Forms.TextBox()
+        Me.lbDays = New System.Windows.Forms.Label()
+        Me.lbAverageOutput = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.AverageUnitsShippedToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.tbUnitsDisplay = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lbUnits
+        '
+        Me.lbUnits.AutoSize = True
+        Me.lbUnits.Location = New System.Drawing.Point(33, 29)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(34, 13)
+        Me.lbUnits.TabIndex = 0
+        Me.lbUnits.Text = "Units:"
+        '
+        'tbUnits
+        '
+        Me.tbUnits.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.tbUnits.Location = New System.Drawing.Point(73, 26)
+        Me.tbUnits.Name = "tbUnits"
+        Me.tbUnits.Size = New System.Drawing.Size(67, 20)
+        Me.tbUnits.TabIndex = 1
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.tbUnits, "Enter Units for the Day")
+        '
+        'lbDays
+        '
+        Me.lbDays.AutoSize = True
+        Me.lbDays.Location = New System.Drawing.Point(168, 29)
+        Me.lbDays.Name = "lbDays"
+        Me.lbDays.Size = New System.Drawing.Size(35, 13)
+        Me.lbDays.TabIndex = 2
+        Me.lbDays.Text = "Day 1"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lbDays, "Displays Current Day")
+        '
+        'lbAverageOutput
+        '
+        Me.lbAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAverageOutput.Location = New System.Drawing.Point(33, 185)
+        Me.lbAverageOutput.Name = "lbAverageOutput"
+        Me.lbAverageOutput.Size = New System.Drawing.Size(215, 30)
+        Me.lbAverageOutput.TabIndex = 4
+        Me.lbAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lbAverageOutput, "Average Unit per Day")
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(23, 226)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnEnter, "Press to enter data")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(185, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnExit, "Press to exit the form")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(104, 226)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnReset, "Press to reset the form")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'tbUnitsDisplay
+        '
+        Me.tbUnitsDisplay.Location = New System.Drawing.Point(23, 50)
+        Me.tbUnitsDisplay.Multiline = True
+        Me.tbUnitsDisplay.Name = "tbUnitsDisplay"
+        Me.tbUnitsDisplay.ReadOnly = True
+        Me.tbUnitsDisplay.Size = New System.Drawing.Size(232, 132)
+        Me.tbUnitsDisplay.TabIndex = 8
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.tbUnitsDisplay, "Units per Day Display")
+        '
+        'ftmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.tbUnitsDisplay)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lbAverageOutput)
+        Me.Controls.Add(Me.lbDays)
+        Me.Controls.Add(Me.tbUnits)
+        Me.Controls.Add(Me.lbUnits)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "ftmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbUnits As Label
+    Friend WithEvents tbUnits As TextBox
+    Friend WithEvents lbDays As Label
+    Friend WithEvents lbAverageOutput As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents AverageUnitsShippedToolTip As ToolTip
+    Friend WithEvents tbUnitsDisplay As TextBox
+End Class
---
+++ AverageUnitsShipped/Form1.vb
@@ -0,0 +1,132 @@
+﻿'Name:                Average Units Shipped
+'Author:              Nicholas Jesseau
+'Date:                Feburary 7th, 2017
+'Description:         A windows Form that is used to calculate the average
+'                     units shipped per day for 7 days, with Validation.
+'                     Then display the output to the user.
+Option Strict On
+Public Class ftmAverageUnitsShipped
+
+    'Declarations'
+    'Constants
+    'Declaring last day
+    Private lastDay As Integer = 7
+    'Variables
+    'Declaring for looping through the array
+    Private currentIndex As Integer = 0
+    'Declaring total for math equation
+    Dim totalShipped As Integer = 0
+    'The output of the math equation is stored here
+    Dim averageShipped As Double = 0
+    'Used for input validation
+    Dim inputUnits As Integer = 0
+    'Declaring the Array
+    Private arrayUnits(6) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'Code for the exit button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Close the form
+        Me.Close()
+    End Sub
+
+    'Code for the reset button
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        'Reset the output text box
+        tbUnitsDisplay.ResetText()
+        'Reset the average output
+        lbAverageOutput.ResetText()
+        'Clearing the total shipped
+        totalShipped = 0
+        'Resets the day to Day 1
+        lbDays.Text = "Day 1"
+
+        'For loop to reset the array
+        For index As Integer = 0 To arrayUnits.Length - 1
+
+            'Setting each element to 0
+            arrayUnits(index) = 0
+
+        Next
+
+        'Setting enter button to active
+        btnEnter.Enabled = True
+        'Setting text box entry to active
+        tbUnits.Enabled = True
+
+        'setting the index to 0
+        currentIndex = 0
+
+        'Resets and focuses on input textbox
+        tbUnits.ResetText()
+        tbUnits.Focus()
+
+
+    End Sub
+
+    'Code for the enter button
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Input'
+        'If statement to validate interger
+        If (Not Integer.TryParse(tbUnits.Text, inputUnits)) Then
+            'Message box displaying error message
+            MessageBox.Show(" The number entered was not a whole number.")
+            'Selecting the text and highlighting it
+            tbUnits.SelectionStart = 0
+            tbUnits.SelectionLength = tbUnits.Text.Length
+
+            'If statement to validate range
+        ElseIf ((inputUnits < 0) Or (inputUnits > 1000)) Then
+            'Message box displaying error message
+            MessageBox.Show("The number entered was not in the appropriate range.")
+            'Selecting the text and highlighting it
+            tbUnits.SelectionStart = 0
+            tbUnits.SelectionLength = tbUnits.Text.Length
+
+        Else
+            'If statement to populate the array
+            If currentIndex <= lastDay - 1 Then
+                'Entering data into the array
+                arrayUnits(currentIndex) = Convert.ToInt32(tbUnits.Text)
+                'Adding the input to display
+                tbUnitsDisplay.Text += arrayUnits(currentIndex) & vbCrLf
+                'incrementing the index 
+                currentIndex += 1
+                'clearing text box inbetween days
+                tbUnits.Clear()
+
+                'Incrementing the days
+                lbDays.Text = "Day " & Convert.ToString(currentIndex + 1)
+
+                'If statement to start processing
+                If currentIndex > lastDay - 1 Then
+
+                    'Changing Days label to average
+                    lbDays.Text = "Average"
+                    'Setting enter button inactive
+                    btnEnter.Enabled = False
+                    'Setting text box units inactive
+                    tbUnits.Enabled = False
+
+                    'Processing'
+                    'For loop to add the array
+                    For index As Integer = 0 To arrayUnits.Length - 1
+                        'Math for adding the array indexs
+                        totalShipped = totalShipped + arrayUnits(index)
+
+                    Next
+                    'Dividing by array length
+                    averageShipped = totalShipped / lastDay
+
+                    'Output'
+                    lbAverageOutput.Text = "Average per day: " & averageShipped.ToString("n2")
+                End If
+            End If
+        End If
+    End Sub
+End Class
